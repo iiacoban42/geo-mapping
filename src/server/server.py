69cb847be4b4055cf3f61cd702f4052445da7821
Andrei Stefan <A.Stefan@student.tudelft.nl>
2020-05-04 17:41:26 +0200

    Adjust pylint rules and reduce warnings

--- src/server/server.py
+++ src/server/server.py
@@ -1,25 +1,30 @@
+"""Module for the server"""
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
 
 PORT = 8000
 
+"""Basic server class"""
+
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
+    """GET method"""
     def do_GET(self):
         try:
-            f = open("../index.html", "rb")
+            file = open("../index.html", "rb")
 
             self.send_response(200)
             self.send_header("Content - type", "text / html")
             self.end_headers()
-            self.wfile.write(f.read())
+            self.wfile.write(file.read())
 
-            f.close()
+            file.close()
             return
         except IOError:
             self.send_error(404, "File Not Found: % s" % self.path)
 
+    """POST method"""
     def do_POST(self):
         content_length = int(self.headers["Content-Length"])
         body = self.rfile.read(content_length)
