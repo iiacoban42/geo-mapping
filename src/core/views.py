98944877c88a0cebec997c5588d57c62d2396707
iiacoban <i.iacoban@student.tudelft.nl>
2020-05-12 12:54:01 +0200

    Add static files to django

--- src/core/views.py
+++ src/core/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def home(request):
-	return render(request, 'accounts/main.html')
-
-
+    return render(request, 'maps/main.html')
