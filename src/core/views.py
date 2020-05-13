9d6138ffa726e2252506a3c3f3afc15d92570904
Georgi Genkov <g.genkov@student.tudelft.nl>
2020-05-13 16:28:07 +0200

    Fix pylint

--- src/core/views.py
+++ src/core/views.py
@@ -1,7 +1,8 @@
 """Views module"""
+import random
+
 from django.shortcuts import render
 from django.http import JsonResponse
-import random
 
 # Create your views here.
 
@@ -13,14 +14,15 @@ def captcha(request):
     """render captcha.html page"""
     return render(request, 'captcha/captcha.html')
 
+# pylint: disable=[unused-argument, fixme]
 def get_tile(request):
     """Return object containing: year, x, y"""
     year = 2010 # TODO: Support other years
-    x = -1
-    y = -1
+    x_coord = -1
+    y_coord = -1
 
     if year == 2010:
-        x = random.choice(range(75079,75804))
-        y = random.choice(range(74990,76586))
+        x_coord = random.choice(range(75079, 75804))
+        x_coord = random.choice(range(74990, 76586))
 
-    return JsonResponse({'year': year, 'x': x, 'y': y})
+    return JsonResponse({'year': year, 'x': x_coord, 'y': y_coord})
