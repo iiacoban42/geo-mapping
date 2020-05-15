7a7a287463314b9b81d596d1117cdc44cbacbee8
iiacoban <i.iacoban@student.tudelft.nl>
2020-05-15 19:31:09 +0200

    Organize python scripts

--- src/scripts/colour_detector.py
+++ src/scripts/colour_detector.py
@@ -1,11 +1,12 @@
 """Module for extracting colors from the tiles"""
-import extcolors
 import urllib.request
+import extcolors
 
 
+# pylint: disable=C0301
 def extract_colors():
-    """Run extcolors on the input image (now it runs on tile.jpg to exemplify behavior)"""
-    colors, pixel_count = extcolors.extract("tile.jpg")
+    """Run extcolors on the input image (now it runs on black_tile.jpg to exemplify behavior)"""
+    colors, pixel_count = extcolors.extract("tile.png")
     # extcolors.print_result(colors, pixel_count)
     result = []
     for (color, pixels) in colors:
@@ -14,17 +15,19 @@ def extract_colors():
 
 
 def get_tile(year, result):
+    """Fetching map tiles"""
     for x_coord in range(75077, 75825):
         for y_coord in range(74956, 75879):
             res = "https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_" + str(
                 year) + "/MapServer/tile/11/" + str(y_coord) + "/" + str(x_coord)
-            urllib.request.urlretrieve(res, "tile.jpg")
+            urllib.request.urlretrieve(res, "tile.png")
             tile = extract_colors()
             print(tile)
             result.append(tile)
 
 
 def get_map(year):
+    """Fetch tiles from map and extract the colors"""
     result_year = []
 
     get_tile(year, result_year)
