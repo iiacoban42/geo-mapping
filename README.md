0ba17e818482630540babf8034b55c5b56b1432c
Georgi Genkov <g.genkov@student.tudelft.nl>
2020-05-26 22:52:19 +0200

    Server verifies CAPTCHAs. Still doesn't remember the unidentified tile.

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
-# Time Travel Maps
+# Time Travel Trough Maps
+The main driving aspect of this project is the fact that the maps of the Netherlands could be used to provide insights into human development, leading to a better understanding of urban planning for the future. Despite already being available online, they are merely images of maps and as such cannot provide too much meaningful information, much less be used for actual research. Had they been digitized though, the story would be completely different.  
 
-
-## How to run server:
+This leads us to what we are trying to accomplish. We are setting the basis for a system that gathers data from its users and then uses that data (along with some simple predictions based on colours) to train a deep learning algorithm that can recognize elements on a map. From the shapes of rivers and cities, to more specific symbols such as churches and oil tanks, the time travelling algorithm will eventually be able to tell them all apart.
+## Prerequisites
+Python 3.7 interpreter  
+Computer  
+Interest in maps  
+## Launching
 1) Open the directory in command prompt (or bash if using Linux).
-2) Type in ` pip install -r 'requirements.txt'` to install the necessary packages. (First time only)
+2) Type in `pip install -r requirements.txt` to install the necessary packages. (First time only)
 3) Type in `python src/manage.py runserver` to run the server.
+4) Open a browser and access `localhost:8000`
+## Built With
+Python with Django - Back-end API, testing   
+HTML, CSS, JavaScript - Front-end, CAPTCHAs  
+Pip, npm - Dependency Management  
+MySQL - Database  
+## Details
+Python has been a strong candidate from the very beginning, since machine learning was involved. The need for a framework that could build a website and handle the security pushed us towards using Django. The fact that Django can also manage test suites was a welcome bonus. On the topic of testing, even though several static analysis tools were used, the main contribution in terms of code quality came from pylint and its high customizability. GitLab's Continuous Integration / Continuous Delivery pipeline helped us make sure that we were not introducing new bugs when we made a commit.  
+
+Since a website was required, it was clear that the trio of HTML, CSS and JavaScript would be a part of our project. The main map viewer window is built using simple HTML and JavaScript to load and display the maps, and its minimalistic look was achieved through CSS. The CAPTCHA system required more attention, since the JavaScript used was more complex, but it kept the minimalistic features through the use of CSS. 
+
+Pip was a useful tool with regards to dependency management. Having all of the required libraries in one file, along with commands for easily installing them saved us a lot of time.  
+
+The choice of a database was quite clear from the very beginning. Since we were not dealing with large amounts of data, a SQL system was considered to be enough. 99% of the time, saving an image file in a database is a bad idea. As such, we decided to store information such as the year and coordinates, since these can be used to uniquely identify a tile in a map and therefore, when a tile was needed, it could be retrieved.  
+
+The initial tile labels were assigned based on the colours present in the tiles. The library used for this is called extcolours. Given an existing image file's name as input, it counts how many times a pixel of each colour appears in the image. Using this, the label was determined by looking at the most dominant colour. Some features such as water or building presence were deemed important enough so that even a small amount of the colours that define them would lead to their labels being assigned as well.
+## Authors
+Boriss Berman  
+Malina Bulina  
+Georgi Genkov  
+Paula Iacoban  
+Andrei Stefan
--- src/static/js/captcha.js
+++ src/static/js/captcha.js
@@ -1,3 +1,98 @@
+var year_1, x_1, y_1, year_2, x_2, y_2;
+
+async function startChallenge(){
+    await fetchImages();
+    document.getElementById("loading").classList.toggle("loader")
+}
+
+async function nextChallenge(){
+    toggleChallenge(1);
+    toggleChallenge(2);
+}
+
+async function submitChallenge(){
+    toggleChallenge(2);
+
+    var building1 = document.getElementById("building1").checked;
+    var water1 = document.getElementById("water1").checked;
+    var land1 = document.getElementById("land1").checked;
+    var church1 = document.getElementById("church1").checked;
+    var oiltank1 = document.getElementById("oiltank1").checked; 
+    
+    var building2 = document.getElementById("building2").checked;
+    var water2 = document.getElementById("water2").checked;
+    var land2 = document.getElementById("land2").checked;
+    var church2 = document.getElementById("church2").checked;
+    var oiltank2 = document.getElementById("oiltank2").checked;
+
+    data = 
+        [{'year': year_1, 'x': x_1, 'y': y_1, 'building': building1, 'water': water1, 'land': land1, 'church': church1, 'oiltank': oiltank1},
+        {'year': year_2, 'x': x_2, 'y': y_2, 'building': building2, 'water': water2, 'land': land2, 'church': church2, 'oiltank': oiltank2}]
+                
+    fetch("/submit_captcha/", {
+        method: "POST", 
+        body: JSON.stringify(data),
+        credentials: 'include' //
+      }).then(response => {
+        var result = document.getElementById("result");
+        if(response.status === 200){
+            result.innerHTML = "Correct";
+            document.getElementById("loading").classList.toggle("loader")
+            document.getElementById("checkmark").classList.toggle("checkmark_success")
+        } else {
+            console.log(response)
+            result.innerHTML = "Incorrect " + response.statusText;
+        }
+      })
+      
+}
+
+async function fetchImages(){
+    const response = await fetch('/get_tile');
+    const json = await response.json();
+
+    console.log(json);
+
+    year_1 = json[0].year;
+    x_1 = json[0].x;
+    y_1 = json[0].y;
+    
+    year_2 = json[1].year;
+    x_2 = json[1].x;
+    y_2 = json[1].y;
+
+    var image1 = document.getElementById("ch_img1")
+
+    loadImage(image1, "https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_" + year_1 + "/MapServer/tile/11/" + y_1 + "/" + x_1)
+    .then(img =>
+        {
+            // If the image is empty (transparent) load a new one, otherwise load the second one
+            var pixelAlpha = getPixel(image1, 1, 1)[3]
+            if(pixelAlpha == 0) {
+                fetchImages();
+            } else {
+              var image2 = document.getElementById("ch_img2")
+                loadImage(image2, "https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_" + year_2 + "/MapServer/tile/11/" + y_2 + "/" + x_2)
+                .then(img =>
+                    {
+                         // If the image is empty (transparent) load a new one, otherwise show the first one
+                        var pixelAlpha = getPixel(image2, 1, 1)[3]
+                        if(pixelAlpha == 0) {
+                            fetchImages();
+                        } else {
+                            toggleChallenge(1);
+                        }
+                    }
+                ).catch(error => fetchImages())
+            }
+        }).catch(error => fetchImages())
+}
+
+function toggleChallenge(id){
+    var popup = document.getElementById("challenge" + id);
+    popup.classList.toggle("show"); 
+}
+
 function getPixel(img, x, y) {
     var canvas = document.createElement('canvas');
     var context = canvas.getContext('2d');
@@ -5,54 +100,13 @@ function getPixel(img, x, y) {
     return context.getImageData(x, y, 1, 1).data;
 }
 
-async function fetchImage(){
-    const response = await fetch('/get_tile');
-    const json = await response.json();
-
-    console.log(json);
-
-    var image1 = document.getElementById("ch_img1")
-    image1.src = "https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_" + json[0].year + "/MapServer/tile/11/" + json[0].y + "/" + json[0].x;
-    image1.crossOrigin = '';
-
-    // If the image is empty (transparent), load a new one
-    image1.onload = function(){
-        var pixelAlpha = getPixel(image1, 1, 1)[3]
-        if(pixelAlpha == 0) fetchImage();
-    }
-
-    // If the image fails to load, load a new one
-    image1.onerror = function(){
-        fetchImage();
-    }
-
-    var image2 = document.getElementById("ch_img2")
-    image2.src = "https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_" + json[1].year + "/MapServer/tile/11/" + json[1].y + "/" + json[1].x;
-    image2.crossOrigin = '';
-
-     // If the image is empty (transparent), load a new one
-    image2.onload = function(){
-        var pixelAlpha = getPixel(image2, 1, 1)[3]
-        if(pixelAlpha == 0) fetchImage();
-    }
-
-    // If the image fails to load, load a new one
-    image2.onerror = function(){
-        fetchImage();
-    }
-}
-
-async function check(){
-    await fetchImage();
-
-    var popup = document.getElementById("challenge1");
-    popup.classList.toggle("show"); 
-}
-
-async function next(){
-    var popup1 = document.getElementById("challenge1");
-    popup1.classList.toggle("show");
-
-    var popup2 = document.getElementById("challenge2");
-    popup2.classList.toggle("show");
+function loadImage(img, url) {
+    return new Promise((resolve, reject) => {
+      img.addEventListener('load', e => resolve(img));
+      img.addEventListener('error', () => {
+        reject(new Error(`Failed to load image's URL: ${url}`));
+      });
+      img.src = url;
+      img.crossOrigin = ''; // Required to get pixel color of the images
+    });
 }
