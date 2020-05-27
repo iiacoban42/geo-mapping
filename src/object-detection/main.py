6913fa9a117b6b46d3d4baa8107a99c154e95411
Boris Berman <b.bermans@student.tudelft.nl>
2020-05-27 18:10:20 +0200

    refactor and introduce validation
    Former-commit-id: 22b08290fda225b509928d4a04db42a9b3084ffa

---
+++ src/object-detection/main.py
@@ -0,0 +1,64 @@
+import os
+
+from detecto import core, utils, visualize
+
+# Constants used for training and evaluation
+TRAIN_DIR = "data/"
+OBJECTS_TO_DETECT = ['church']
+DETECTION_THRESHOLD = 0.95
+
+
+def train_model():
+    """Train church detection model"""
+    dataset = core.Dataset(TRAIN_DIR)
+    model = core.Model(OBJECTS_TO_DETECT)
+
+    model.fit(dataset)
+    model.save('model_weights.pth')
+
+
+def main():
+    """Load model and evaluate it"""
+    print("Loading object-detection model...")
+    model = core.Model.load('model/model_weights.pth', OBJECTS_TO_DETECT)
+
+    correct_classifications = 0
+
+    # Check church detection
+    for i, filename in enumerate(os.listdir("validation-data/church")):
+        if filename.endswith(".png"):
+            image = utils.read_image('validation-data/church/{}'.format(filename))
+            predictions = model.predict(image)
+
+            # predictions format: (labels, boxes, scores)
+            labels, boxes, scores = predictions
+            filtered_scores = list(filter(lambda score: score > DETECTION_THRESHOLD, scores))
+
+            # increment correct classifications if churches were found
+            if len(filtered_scores) > 0:
+                correct_classifications += 1
+
+            print("Scores for church image {} are {}".format(i, filtered_scores))
+
+    # Check absence of churches
+    for i, filename in enumerate(os.listdir("validation-data/non-church")):
+        if filename.endswith(".png"):
+            image = utils.read_image('validation-data/non-church/{}'.format(filename))
+            predictions = model.predict(image)
+
+            # predictions format: (labels, boxes, scores)
+            labels, boxes, scores = predictions
+            filtered_scores = list(filter(lambda score: score > DETECTION_THRESHOLD, scores))
+
+            # increment correct classifications if churches were not found
+            if len(filtered_scores) == 0:
+                correct_classifications += 1
+
+            print("Scores for non-church image {} are {}".format(i, filtered_scores))
+
+    image_count = len(os.listdir("validation-data/non-church")) + len(os.listdir("validation-data/church"))
+    print("Accuracy of this model is {}%".format(correct_classifications / image_count * 100))
+
+
+if __name__ == "__main__":
+    main()
