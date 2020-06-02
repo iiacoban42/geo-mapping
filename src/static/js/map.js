61949f7a4dc9ebaa1697eb8dbb19896234987a56
Malina Bulina <m.a.bulina-1@student.tudelft.nl>
2020-06-02 16:52:39 +0200

    ADD LAYERSgit add .
    Former-commit-id: b59db082bd7de032778a155384a83820071a8d4c

--- src/static/js/map.js
+++ src/static/js/map.js
@@ -1,20 +1,25 @@
 var map;
-require(["esri/map", "esri/layers/ArcGISTiledMapServiceLayer", "dojo/domReady!"],
-    function (Map, Tiled) {
+require(["esri/Map", "esri/WebMap", "esri/views/MapView", "esri/layers/TileLayer", "esri/layers/FeatureLayer", "dojo/domReady!"],
+    function (Map, WebMap, MapView, TileLayer, FeatureLayer) {
+        var base = new TileLayer({
+            url: "https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_2016/MapServer"
+        });
         map = new Map("map", {
             center: [-122.45, 37.75],
         });
-        var tiled = new Tiled("https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_2016/MapServer");
-        map.addLayer(tiled);
-    }
-);
+        map.add(base)
+        var layer = new FeatureLayer({
+            url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/netherlands/FeatureServer/9",
+            outFields: ["*"]
+        });
+        console.log(layer.toString() + "  AAA " + layer.visibility)
+        map.add(layer);
+        var view = new MapView({
+            container: "map",
+            map: map
+        });
 
-function changeYear(year) {
-    console.log(year)
-    var baseMapLayer = new esri.layers.ArcGISTiledMapServiceLayer("https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_" + year + "/MapServer");
-    map.removeAllLayers();
-    map.addLayer(baseMapLayer);
-}
+    });
 
 /* Open when someone clicks on the span element */
 function openNav() {
@@ -40,12 +45,19 @@ btn_overview.onclick = function () {
     console.log("tiles_overview")
     location.assign('/tiles_overview/');
 }
-
 $(document).ready(function () {
-    $(menu).click(function (event) {
-        if (event.target.id !== 'menu') {
-            changeYear(event.target.id);
-            document.getElementById("current").innerHTML = event.target.id;
-        }
-    });
-});
+        $(menu).click(function (event) {
+            if (event.target.id !== 'menu') {
+                var year = event.target.id
+
+                console.log(year)
+                var layer = new es.layers.TileLayer({
+                    url: "https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_" + year + "/MapServer"
+                });
+                map.removeAll();
+                map.add(layer);
+                document.getElementById("current").innerHTML = event.target.id;
+            }
+        });
+    }
+);
