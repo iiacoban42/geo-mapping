e51a0e5920f80d3f4fde75a9cd6e6d26e7930bdb
Malina Bulina <m.a.bulina-1@student.tudelft.nl>
2020-06-02 21:09:03 +0200

    add one point on map
    Former-commit-id: 04545a98a0e9084f3b36c4d6b498e83566451940

--- src/static/js/map.js
+++ src/static/js/map.js
@@ -1,6 +1,7 @@
 var map;
-require(["esri/Map", "esri/WebMap", "esri/views/MapView", "esri/layers/TileLayer", "esri/layers/FeatureLayer", "dojo/domReady!"],
-    function (Map, WebMap, MapView, TileLayer, FeatureLayer) {
+var view;
+require(["esri/Map", "esri/views/MapView", "esri/layers/TileLayer", "esri/Graphic", "dojo/domReady!"],
+    function (Map, MapView, TileLayer, Graphic) {
         var baseLayer = new TileLayer({
             url: "https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_2016/MapServer"
         });
@@ -8,15 +9,60 @@ require(["esri/Map", "esri/WebMap", "esri/views/MapView", "esri/layers/TileLayer
             center: [-122.45, 37.75],
         });
         map.add(baseLayer)
-        var buildingsLayer = new FeatureLayer({
-            url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/netherlands/FeatureServer/9",
-            outFields: ["*"]
-        });
-        map.add(buildingsLayer);
-        var view = new MapView({
+
+        view = new MapView({
             container: "map",
             map: map
         });
+
+        // First create a point geometry
+        var point = {
+            type: "point",                     // autocasts as new Point()
+            longitude: 4.470590,
+            latitude: 51.922910
+        };
+
+        // Create a symbol for drawing the point
+        var pointSymbol = {
+            type: "simple-marker",             // autocasts as new SimpleMarkerSymbol()
+            color: [226, 119, 40],
+            width: 8,
+            outline: {
+                color: [255, 255, 255],
+                width: 4
+            }
+        };
+
+        // Create an object for storing attributes related to the point
+        var pointLabels = {
+            Name: "Mali",
+            Where: "Rotterdam",
+        };
+
+        var pointGraphic = new Graphic({
+            geometry: point,
+            symbol: pointSymbol,
+            attributes: pointLabels,
+            popupTemplate: {
+                title: "{Name}",
+                content: [
+                    {
+                        type: "fields",
+                        fieldInfos: [
+                            {
+                                fieldName: "Name"
+                            },
+                            {
+                                fieldName: "Where"
+                            }
+                        ]
+                    }
+                ]
+            }
+        });
+
+        // Add the line graphic to the view's GraphicsLayer
+        view.graphics.add(pointGraphic);
 
         $(document).ready(function () {
                 $(menu).click(function (event) {
@@ -33,7 +79,8 @@ require(["esri/Map", "esri/WebMap", "esri/views/MapView", "esri/layers/TileLayer
                 });
             }
         );
-    });
+    }
+);
 
 /* Open when someone clicks on the span element */
 function openNav() {
