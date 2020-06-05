a12c7818c7792d351854eb0bc11ccd60bfb741cf
Andrei Stefan <A.Stefan@student.tudelft.nl>
2020-06-05 12:18:30 +0200

    refactor parameters for better accuracy
    Former-commit-id: 13424b74c7caa5d170e1fbd6a6f9bea25b0f3cea

--- src/scripts/detection.py
+++ src/scripts/detection.py
@@ -14,14 +14,14 @@ from keras.preprocessing.image import ImageDataGenerator
 
 IMG_SIZE = 256
 NB_CHANNELS = 3
-BATCH_SIZE = 1
-NB_TRAIN_IMG = 1560
-NB_VALID_IMG = 300
+BATCH_SIZE = 2
+NB_TRAIN_IMG = 6913
+NB_VALID_IMG = 3813
 
 cnn = Sequential()
 cnn.add(Conv2D(filters=32,
-               kernel_size=(2,2),
-               strides=(1,1),
+               kernel_size=(8,8),
+               strides=(2,2),
                padding='same',
                input_shape=(IMG_SIZE,IMG_SIZE,NB_CHANNELS),
                data_format='channels_last'))
@@ -29,38 +29,52 @@ cnn.add(Activation('relu'))
 cnn.add(MaxPooling2D(pool_size=(2,2),
                      strides=2))
 cnn.add(Conv2D(filters=64,
+               kernel_size=(4,4),
+               strides=(1,1),
+               padding='valid',
+               # input_shape=(IMG_SIZE,IMG_SIZE,NB_CHANNELS),
+               ))
+cnn.add(Activation('relu'))
+cnn.add(MaxPooling2D(pool_size=(2,2),
+                     strides=1))
+cnn.add(Conv2D(filters=64,
                kernel_size=(2,2),
                strides=(1,1),
                padding='valid'))
 cnn.add(Activation('relu'))
 cnn.add(MaxPooling2D(pool_size=(2,2),
                      strides=2))
+
+cnn.add(Conv2D(filters=64,
+               kernel_size=(2,2),
+               strides=(1,1),
+               padding='valid',
+               # input_shape=(IMG_SIZE,IMG_SIZE,NB_CHANNELS),
+               ))
+cnn.add(Activation('relu'))
+cnn.add(MaxPooling2D(pool_size=(2,2),
+                     strides=2))
+
 cnn.add(Flatten())
 cnn.add(Dense(64))
 cnn.add(Activation('relu'))
-cnn.add(Dropout(0.25))
-cnn.add(Dense(1))
-cnn.add(Activation('sigmoid'))
+cnn.add(Dropout(0.35))
+cnn.add(Dense(3))
+cnn.add(Activation('softmax'))
 cnn.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
 train_datagen = ImageDataGenerator(
-    rotation_range = 40,
-    width_shift_range = 0.2,
-    height_shift_range = 0.2,
-    rescale = 1./255,
-    shear_range = 0.2,
-    zoom_range = 0.2,
     horizontal_flip = True)
 validation_datagen = ImageDataGenerator(rescale = 1./255)
 train_generator = train_datagen.flow_from_directory(
     'train',
     target_size=(IMG_SIZE,IMG_SIZE),
-    class_mode='binary',
+    class_mode='categorical',
     batch_size = BATCH_SIZE)
 validation_generator = validation_datagen.flow_from_directory(
     'validation',
     target_size=(IMG_SIZE,IMG_SIZE),
-    class_mode='binary',
+    class_mode='categorical',
     batch_size = BATCH_SIZE)
 
 start = time.time()
