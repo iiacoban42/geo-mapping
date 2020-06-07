dcae453849428e8e46a7bdce256b64d5e91731f8
Malina Bulina <m.a.bulina-1@student.tudelft.nl>
2020-06-07 23:19:58 +0200

    add annotations on maps?
    Former-commit-id: ec1f6de26fd546ecb2effb5a5c505389f315eee2

--- src/static/js/map.js
+++ src/static/js/map.js
@@ -1,14 +1,14 @@
 var map;
 var view;
-require(["esri/Map", "esri/views/MapView", "esri/layers/TileLayer", "esri/Graphic", "esri/geometry/Extent",
-        "esri/widgets/Search", "dojo/domReady!"],
+require(['esri/Map', 'esri/views/MapView', 'esri/layers/TileLayer', 'esri/Graphic', 'esri/geometry/Extent',
+        'esri/widgets/Search', 'dojo/domReady!'],
     function (Map, MapView, TileLayer, Graphic, Extent, Search) {
 
         // initial map
         var baseLayer = new TileLayer({
-            url: "https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_2016/MapServer"
+            url: 'https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_2016/MapServer'
         });
-        map = new Map("map", {
+        map = new Map('map', {
             center: [-122.45, 37.75],
         });
 
@@ -16,13 +16,13 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/TileLayer", "esri/Graphi
 
         // map is added to the view
         view = new MapView({
-            container: "map",
+            container: 'map',
             map: map,
         });
 
         // create a symbol for drawing the point
         var pointSymbol = {
-            type: "simple-marker",             // autocasts as new SimpleMarkerSymbol()
+            type: 'simple-marker',             // autocasts as new SimpleMarkerSymbol()
             color: [226, 119, 40],
             width: 8,
             outline: {
@@ -33,16 +33,16 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/TileLayer", "esri/Graphi
 
         // template for points on map
         var template = {
-            title: "{Label}",
+            title: '{Label}',
             content: [
                 {
-                    type: "fields",
+                    type: 'fields',
                     fieldInfos: [
                         {
-                            fieldName: "Name"
+                            fieldName: 'Name'
                         },
                         {
-                            fieldName: "Other"
+                            fieldName: 'Other'
                         }
                     ]
                 }
@@ -65,47 +65,69 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/TileLayer", "esri/Graphi
         }
 
         // add div element to show coords
-        var coordsWidget = document.createElement("div");
-        coordsWidget.id = "coordsWidget";
-        coordsWidget.className = "esri-widget esri-component";
-        coordsWidget.style.padding = "7px 15px 5px";
-        view.ui.add(coordsWidget, "bottom-right");
+        var coordsWidget = document.createElement('div');
+        coordsWidget.id = 'coordsWidget';
+        coordsWidget.className = 'esri-widget esri-component';
+        coordsWidget.style.padding = '7px 15px 5px';
+        view.ui.add(coordsWidget, 'bottom-right');
 
         // update lat, lon, zoom and scale
         // lat and lon are in other coord system for now (hopefully)
         function showCoordinates(event) {
-            var coords = "Lat/Lon (wrong for now?) " + event.y + " " + event.x +
-                " | Scale 1:" + Math.round(view.scale * 1) / 1 +
-                " | Zoom " + view.zoom;
+            var coords = 'Lat/Lon (wrong for now?) ' + event.y + ' ' + event.x +
+                ' | Scale 1:' + Math.round(view.scale * 1) / 1 +
+                ' | Zoom ' + view.zoom;
             coordsWidget.innerHTML = coords;
         }
 
         // add event and show center coordinates after the view is finished moving e.g. zoom, pan
-        view.watch(["stationary"], function () {
+        view.watch(['stationary'], function () {
             showCoordinates(view.center);
         });
 
         // add event to show mouse coordinates on click and move
-        view.on(["pointer-down"], function (evt) {
+        view.on(['pointer-down'], function (evt) {
             showCoordinates(view.toMap({x: evt.x, y: evt.y}));
         });
 
-        view.on("click", function (event) {
+
+        view.on('click', function (event) {
             event.stopPropagation(); // overwrite default click-for-popup behavior
             // Get the coordinates of the click on the view
+            year = document.getElementById('current').innerHTML
             coord_x_db = Math.round((event.mapPoint.x + 30527385.66843) / 406.55828)
             coord_y_db = Math.round((event.mapPoint.y - 31113121.21698) / (-406.41038))
 
+            const req = {'year': year, 'x_coord': coord_x_db, 'y_coord': coord_y_db}
+            const response = fetch('/get_labels/' + req);
+            const json = response.json();
+
+            // template for labelled tiles on map
             view.popup.open({
-                // Set the popup's title to the coordinates of the location
-                title: "Tile from year " + document.getElementById("current").innerHTML + " with coords x= " + coord_x_db + ", y= " + coord_y_db + "!!!",
+                title: 'Tile from year ' + year + ' with coords x= ' + coord_x_db + ', y= ' + coord_y_db + '!!!',
+                content: [
+                    {
+                        type: 'fields',
+                        fieldInfos: [
+                            {
+                                building: json.building
+                            },
+                            {
+                                land: json.land
+
+                            },
+                            {
+                                water: json.water
+                            },
+                        ]
+                    }
+                ],
                 location: event.mapPoint // Set the location of the popup to the clicked location
             });
             view.popup.content = view.spatialReference.wkid.toString();
         });
-
         var searchWidget = new Search({view: view});
-        view.ui.add(searchWidget, "top-right");
+        view.ui.add(searchWidget, 'top-right');
 
 
         // change years based on the selection from the menu
@@ -115,11 +137,11 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/TileLayer", "esri/Graphi
                     var year = event.target.id
                     console.log(year)
                     var yearLayer = new TileLayer({
-                        url: "https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_" + year + "/MapServer"
+                        url: 'https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_' + year + '/MapServer'
                     });
                     map.removeAll();
                     map.add(yearLayer);
-                    document.getElementById("current").innerHTML = event.target.id;
+                    document.getElementById('current').innerHTML = event.target.id;
                 }
                 });
             }
@@ -129,26 +151,26 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/TileLayer", "esri/Graphi
 
 // open when someone clicks on the span element
 function openNav() {
-    document.getElementById("open").style.visibility = "hidden";
-    document.getElementById("myNav").style.width = "7%";
+    document.getElementById('open').style.visibility = 'hidden';
+    document.getElementById('myNav').style.width = '7%';
 
 }
 
-// close when someone clicks on the "x" symbol inside the overlay
+// close when someone clicks on the 'x' symbol inside the overlay
 function closeNav() {
-    document.getElementById("open").style.visibility = "visible";
-    document.getElementById("myNav").style.width = "0%";
+    document.getElementById('open').style.visibility = 'visible';
+    document.getElementById('myNav').style.width = '0%';
 }
 
 var btn_captcha = document.getElementById('captcha');
 btn_captcha.onclick = function () {
-    console.log("captcha")
+    console.log('captcha')
     location.assign('/captcha/');
 }
 
 var btn_overview = document.getElementById('overview');
 btn_overview.onclick = function () {
-    console.log("tiles_overview")
+    console.log('tiles_overview')
     location.assign('/tiles_overview/');
 }
 
