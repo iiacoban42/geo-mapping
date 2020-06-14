bee9a33f8b849c3efbb7c8ad33e0e17efe78730b
Boris Berman <b.bermans@student.tudelft.nl>
2020-06-14 17:07:26 +0200

    save predictions
    Former-commit-id: 21819430689a76aed0a97e53d0611cb5dcbdc3f3

--- src/object_detection/evaluate.py
+++ src/object_detection/evaluate.py
@@ -1,8 +1,10 @@
 import os
+import django
+django.setup()
 
 from detecto import core, utils
+from core.models import AI_Objects as AIObjectsTable
 from core.models import AI_Tiles as AITilesTable
-os.chdir('object_detection')
 
 # Constants used for evaluation
 OBJECTS_TO_DETECT = ['church']
@@ -10,12 +12,37 @@ DETECTION_THRESHOLD = 0.95
 
 # saves ai classification in the database
 def save_labels():
-    tile = AITilesTable(x_coord=75065, y_coord=75510, year=2016)
-    print(tile)
-    # tile.save()
-    # prediction = PredictionsTable(tiles_id=tile, water_prediction=water, land_prediction=land,
-    #                               buildings_prediction=building)
-    # prediction.save()
+    model = core.Model.load('model/model_weights.pth', OBJECTS_TO_DETECT)
+    for filename in os.listdir("data"):
+        separated_name = filename.split('_')
+        if separated_name[2] == ".png":
+            image = utils.read_image('data/{}'.format(filename))
+            predictions = model.predict(image)
+
+            # Save tile
+            tile = AITilesTable(x_coord=int(separated_name[0]), y_coord=int(separated_name[1]), year=2016)
+            tile.save()
+
+            # Save prediction
+            score = int(predictions[2][0] * 100)
+            prediction = AIObjectsTable(tiles_id=tile, type="church",prediction=score)
+            prediction.save()
+            print(filename + "successfully saved & predicted")
+
+    for filename in os.listdir("validation_data/church"):
+        separated_name = filename.split('_')
+        image = utils.read_image('validation_data/church/{}'.format(filename))
+        predictions = model.predict(image)
+
+        # Save tile
+        tile = AITilesTable(x_coord=int(separated_name[0]), y_coord=int(separated_name[1]), year=2016)
+        tile.save()
+
+        # Save prediction
+        score = int(predictions[2][0] * 100)
+        prediction = AIObjectsTable(tiles_id=tile, type="church", prediction=score)
+        prediction.save()
+        print(filename + "successfully saved & predicted")
 
 
 def main():
